69cccd784d072f4ba34d8cb252954e343caedca2
Vedant.ccbp <vedantsonawane93@gmail.com>
2026-07-03 19:00:15 +0530

    01

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,8 +1,13 @@
 let name = "Vedant"
-console.log(name)
 let age = 22
-console.log(age)
-const ids = "@123"
-console.log(ids)
+const studentId = 112023133
 let city = "Pune"
-console.log(city)
+let number = 12345
+let bigNum = 123456789987456321
+let isAdult = true
+
+// value of variables
+console.table([name, age, studentId, city, number, bigNum, isAdult])
+
+// type of data that variables are used
+console.table([typeof name, typeof age, typeof studentId, typeof city, typeof number, typeof bigNum, typeof isAdult])
