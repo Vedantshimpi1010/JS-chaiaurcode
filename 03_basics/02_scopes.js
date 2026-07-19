cae41dd75789e8c211d6c5da39be6b6c7c113e94
Vedant.ccbp <vedantsonawane93@gmail.com>
2026-07-19 10:13:41 +0530

    new basics

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,35 @@
+function one(){
+    const username = "Vedant"
+    
+
+    function two(){
+        const website = "youtube.com"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if(true){
+    const username = "Hitesh"
+    if(username === "Hitesh"){
+        const website = " youtube.com"
+        //console.log(username + website);
+    }
+}   
+
+//************** interesting ****************/
+
+ function addOne(num){
+    return num + 1
+ }
+ addOne(5)
+
+
+ const addTwo = function(num){
+    return num + 2
+ }
+ addTwo(5)
+
+ 
